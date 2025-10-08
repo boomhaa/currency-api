432e0e5398693e3a569c11b2a8a87c5727f19027
Vladislav <90379111+boomhaa@users.noreply.github.com>
2025-10-08 16:50:34 +0300

    some fix

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,21 +1,17 @@
 package com.example
 
 import com.example.di.CurrencyRepoHolder.repo
-import com.example.di.currencyModule
 import com.example.plugin.configureKoin
 import com.example.plugin.configureSerialization
 import com.example.utils.updateScheduler
 import io.ktor.server.application.*
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import org.koin.ktor.plugin.Koin
 
 fun main(args: Array<String>) {
     embeddedServer(Netty, host = "0.0.0.0", port = 8080){
-        install(Koin){
-            modules(currencyModule)
-        }
-
+        configureKoin()
+        configureSerialization()
         configureRouting()
 
         updateScheduler(repo)
